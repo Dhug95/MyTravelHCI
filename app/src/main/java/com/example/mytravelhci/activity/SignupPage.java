3dea045f48eed1925d0f645f346d4dbc8eb37992
CarmineMansueto95 <carminemansueto95@gmail.com>
2019-07-01 18:13:39 +0200

    Merge branch 'master' of /home/carminem/Desktop/MyTravelHCI with conflicts.

--- app/src/main/java/com/example/mytravelhci/activity/SignupPage.java
+++ app/src/main/java/com/example/mytravelhci/activity/SignupPage.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.mytravelhci.R;
+
+import fr.ganfra.materialspinner.MaterialSpinner;
 
 public class SignupPage extends AppCompatActivity {
 
@@ -14,6 +18,11 @@ public class SignupPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
+        String[] ITEMS = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ITEMS);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinner = (MaterialSpinner) findViewById(R.id.currencySpinner);
+        spinner.setAdapter(adapter);
     }
 
     public void startHome(View view) {
