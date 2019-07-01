2bbb6f2f9fdbfe35d0d0306323ca4b2cc8d18cde
Francesco D'Abbraccio <dabbraccio.francesco@gmail.com>
2019-07-01 18:27:56 +0200

    HOME PAGE REWRITTEN

--- app/src/main/java/com/example/mytravelhci/activity/HomePageActivity.java
+++ app/src/main/java/com/example/mytravelhci/activity/HomePageActivity.java
@@ -1,14 +1,18 @@
 package com.example.mytravelhci.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.mytravelhci.R;
 
-public class HomePageActivity extends AppCompatActivity {
+public class HomePageActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
