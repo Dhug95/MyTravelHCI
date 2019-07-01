0b9f2b085e54cf33a70e2211ae587365646e5fa8
Francesco D'Abbraccio <dabbraccio.francesco@gmail.com>
2019-07-01 11:28:07 +0200

    Bianca added

--- app/src/main/java/com/example/mytravelhci/activity/HomePageActivity.java
+++ app/src/main/java/com/example/mytravelhci/activity/HomePageActivity.java
@@ -2,7 +2,9 @@ package com.example.mytravelhci.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.mytravelhci.R;
 
@@ -12,5 +14,10 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+
+    public void goLogIn(View view) {
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
     }
 }
