36c1a5f44f826812a232ff8a71168919f4fcddf9
Francesco D'Abbraccio <dabbraccio.francesco@gmail.com>
2019-07-03 15:36:31 +0200

    Signup page updated

--- app/src/main/java/com/example/mytravelhci/activity/SignupPage.java
+++ app/src/main/java/com/example/mytravelhci/activity/SignupPage.java
@@ -1,9 +1,12 @@
 package com.example.mytravelhci.activity;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -30,6 +33,17 @@ public class SignupPage extends AppCompatActivity {
         spinner.setItems("EUR", "YEN", "US", "UK");
         spinner.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        Drawable upArrow = getResources().getDrawable(R.drawable.ic_arrow_back_white_24dp);
+
+        // Enable the Up button
+        ab.setHomeAsUpIndicator(upArrow);
+        ab.setDisplayHomeAsUpEnabled(true);
 
         /*spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
             @Override
@@ -40,7 +54,7 @@ public class SignupPage extends AppCompatActivity {
     }
 
     public void startHome(View view) {
-        Toast t = Toast.makeText(getApplicationContext(),"Account created!",Toast.LENGTH_LONG);
+        Toast t = Toast.makeText(getApplicationContext(), "Account created!", Toast.LENGTH_LONG);
         t.show();
         Intent intent = new Intent(this, NotLoggedHomeActivity.class);
         startActivity(intent);
