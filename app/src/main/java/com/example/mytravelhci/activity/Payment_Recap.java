04e0d86a809371d668a3d9ca566815058d96a4c5
FedericoBianca <federico.bianca95@gmail.com>
2019-07-08 14:04:09 +0200

    Added payment recap activity and layout

---
+++ app/src/main/java/com/example/mytravelhci/activity/Payment_Recap.java
@@ -0,0 +1,16 @@
+package com.example.mytravelhci.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mytravelhci.R;
+
+public class Payment_Recap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment__recap);
+    }
+}
