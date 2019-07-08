b628ece8c0325c0235ec702c261b18628be60463
Francesco D'Abbraccio <dabbraccio.francesco@gmail.com>
2019-07-08 15:39:21 +0200

    Pay debt finished

--- app/src/main/java/com/example/mytravelhci/activity/PayDebtActivity.java
+++ app/src/main/java/com/example/mytravelhci/activity/PayDebtActivity.java
@@ -1,20 +1,35 @@
 package com.example.mytravelhci.activity;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.mytravelhci.R;
+import com.jaredrummler.materialspinner.MaterialSpinner;
 
 public class PayDebtActivity extends AppCompatActivity {
+
+    TextView receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_debt);
+
+        MaterialSpinner spinner = (MaterialSpinner) findViewById(R.id.currencySpinner);
+        spinner.setItems("Select", "EUR", "JPY", "USD", "GBP");
+        spinner.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+        receiver = findViewById(R.id.receiver);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -29,5 +44,22 @@ public class PayDebtActivity extends AppCompatActivity {
         // Enable the Up button
         ab.setHomeAsUpIndicator(upArrow);
         ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    public void chooseUser(View view) {
+        AlertDialog ad = new AlertDialog.Builder(this)
+                .setTitle("Choose user")
+                .setPositiveButton("Cancel", null)
+                .setItems(R.array.users, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        receiver.setText(getResources().getStringArray(R.array.users)[i]);
+                    }
+                })
+                .show();
+    }
+
+    public void goToPayment(View view) {
     }
 }
